a75a28e6b11bbe89cdcd7ced0ee5b7b3f447cfef
MayankDev-11 <mayank11github@gmail.com>
2020-12-25 19:49:09 +0530

    Created Course-Selling-App

---
+++ courses/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from courses.models import Course
+
+admin.site.register(Course)
+
+# Register your models here.
---
+++ courses/models/course.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100, null=False)
+    description = models.CharField(max_length=500, null=True)
+    price = models.IntegerField(null=False)
+    discount = models.IntegerField(null=False, default=0)
+    active = models.BooleanField(default=False)
+    thumbnail = models.ImageField(upload_to='files/thumbnail')
+    date = models.DateTimeField(auto_now_add=True)
+    resource = models.FileField(upload_to='files/resource')
+    length = models.IntegerField(null=False)
